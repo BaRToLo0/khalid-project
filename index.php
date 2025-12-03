3db76177eb219bc4595e16612d40873f5aafebf2
BaRToLo0 <118033349+BaRToLo0@users.noreply.github.com>
2025-12-04 00:18:46 +0100

    Refactor index.php for modern login page design
    Updated the login and registration forms with modern styling and structure.

--- index.php
+++ index.php
@@ -1,74 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register & Login</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="style.css">
+    <title>Modern Login Page | AsmrProg</title>
 </head>
+
 <body>
-    <div class="container" id="signup" style="display:none;">
-      <h1 class="form-title">Register</h1>
-      <form method="post" action="register.php">
+
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form method="post" action="register.php">
         <div class="input-group">
-           <i class="fas fa-user"></i>
-           <input type="text" name="fName" id="fName" placeholder="First Name" required>
            <label for="fname">First Name</label>
+           <br>
+           <input type="text" name="fName" id="fName" placeholder="First Name" required>
+           
         </div>
         <BR>
         <div class="input-group">
-            <i class="fas fa-user"></i>
-            <input type="text" name="lName" id="lName" placeholder="Last Name" required>
             <label for="lName">Last Name</label>
+            <br>
+            <input type="text" name="lName" id="lName" placeholder="Last Name" required>
+            
         </div>
         <BR>
         <div class="input-group">
-            <i class="fas fa-envelope"></i>
-            <input type="email" name="email" id="email" placeholder="Email" required>
             <label for="email">Email</label>
+            <br>
+            <input type="email" name="email" id="email" placeholder="Email" required>
+            
         </div>
         <BR>
         <div class="input-group">
-            <i class="fas fa-lock"></i>
+           <label for="password">Password</label>
+           <br>
             <input type="password" name="password" id="password" placeholder="Password" required>
-            <label for="password">Password</label>
+            
         </div>
-       <input type="submit" class="btn" value="Sign Up" name="signUp">
+       <button type="submit" class="btn" value="Sign Up" name="signUp">Sign Up</button>
       </form>
       
-      <div class="links">
-        <p>Already Have Account ?</p>
-        <button id="signInButton">Sign In</button>
-      </div>
-    </div>
-
-    <div class="container" id="signIn">
-        <h1 class="form-title">Sign In</h1>
-        <form method="post" action="register.php">
+        </div>
+        <div class="form-container sign-in">
+           <form method="post" action="register.php">
           <div class="input-group">
-              <i class="fas fa-envelope"></i>
-              <input type="email" name="email" id="email" placeholder="Email" required>
               <label for="email">Email</label>
+              <br>
+              <input type="email" name="email" id="email" placeholder="Email" required>
+              
           </div>
           <BR>
           <div class="input-group">
-              <i class="fas fa-lock"></i>
-              <input type="password" name="password" id="password" placeholder="Password" required>
               <label for="password">Password</label>
+              <br>
+              <input type="password" name="password" id="password" placeholder="Password" required>
+              
           </div>
           <BR>
           <p class="recover">
             <a href="#">Recover Password</a>
           </p>
-         <input type="submit" class="btn" value="Sign In" name="signIn">
+         <button type="submit" class="btn" value="Sign In" name="signIn">Log In</button>
         </form>
-        <BR>
-        <div class="links">
-          <p>Don't have account yet?</p>
-          <button id="signUpButton">Sign Up</button>
         </div>
-      </div>
-      <script src="script.js"></script>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Register with your personal details to use all of site features</p>
+                    <button class="hidden" id="register">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
 </body>
+
 </html>
